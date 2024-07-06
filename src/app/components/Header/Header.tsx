3899b0664d1c40315d434850b71a98b90e92dd83
JPSS14 <jp_scarabelli@hotmail.com>
2024-07-06 11:57:18 -0300

    feat: Header component

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import style from "./Header.module.scss";
+
+export const Header = () => {
+  return (
+    <header className={style.header}>
+      <div className={style.image__container}>
+        <Image
+          src="/people-register-logo.png"
+          alt="People Register logo"
+          title="People Register"
+          width={852}
+          height={336}
+        />
+      </div>
+    </header>
+  );
+};
