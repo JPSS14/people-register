8c94b63a70ef20d534e832b7a36b5927dda14d16
JPSS14 <jp_scarabelli@hotmail.com>
2024-07-06 12:06:45 -0300

    feat: add template in app

---
+++ src/app/template.tsx
@@ -0,0 +1,14 @@
+import { Header } from "./components";
+
+export default function HomeTemplate({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <Header />
+      <div>{children}</div>
+    </div>
+  );
+}
