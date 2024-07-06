ba5d0c88fce5683b5a76a457c24e44c23dbb9ec3
JPSS14 <jp_scarabelli@hotmail.com>
2024-07-06 13:05:40 -0300

    test: Header component with correct render

---
+++ src/app/components/Header/Header.test.tsx
@@ -0,0 +1,18 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { Header } from "./Header";
+
+describe("Header component", () => {
+  it("renders the header with the correct logo", () => {
+    render(<Header />);
+
+    const logo = screen.getByAltText("People Register logo");
+    expect(logo).toBeInTheDocument();
+
+    // need use expect.stringContaining() because the Image Next optimization change the src
+    expect(logo).toHaveAttribute(
+      "src",
+      expect.stringContaining("people-register-logo.png")
+    );
+  });
+});
