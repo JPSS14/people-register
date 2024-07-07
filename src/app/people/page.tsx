6be0dafaf84cf3fdba05840be981cfbd46e404b1
JPSS14 <jp_scarabelli@hotmail.com>
2024-07-07 20:15:00 -0300

    feat: add loading in people page

--- src/app/people/page.tsx
+++ src/app/people/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Loading from "react-loading";
 import { PeopleList } from "../components";
 import { Button } from "../components/Button";
 import { usePeopleContext } from "../contexts/people-context";
@@ -7,7 +8,20 @@ export default function People() {
   const { peopleList, handleResetList } = usePeopleContext();
   return (
     <div>
-      {peopleList.length && <PeopleList />}
+      {peopleList.length ? (
+        <PeopleList />
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "calc(100vh - 90px)",
+          }}
+        >
+          <Loading type="spin" color="#00c8b3" />
+        </div>
+      )}
       <div
         style={{ display: "flex", justifyContent: "center", margin: "24px 0" }}
       >
